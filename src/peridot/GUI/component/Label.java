4b78f13b9d2a3445a7fd7004ca5d881633cca104
Pitágoras Alves <alves.pitagoras@gmail.com>
2018-04-03 14:30:24 -0300

    Scaling down images to 800 width when they are too large

--- src/peridot/GUI/component/Label.java
+++ src/peridot/GUI/component/Label.java
@@ -6,10 +6,14 @@
 package peridot.GUI.component;
 
 import peridot.GUI.Aesthetics;
+import peridot.Log;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+
 /**
  *
  * @author Pitágoras Alves
@@ -29,7 +33,26 @@ public class Label extends JLabel {
     }
     
     public static JLabel getImageLabel(File file){
-        ImageIcon imageIcon = new ImageIcon(file.getAbsolutePath());
+        ImageIcon imageIcon;
+        try {
+            imageIcon = new ImageIcon(ImageIO.read(file));
+        }catch (IOException ex){
+            imageIcon = new ImageIcon(file.getAbsolutePath());
+        }
+        //ImageIcon
+        Image image = imageIcon.getImage();
+        if(image.getWidth(null) >= 800){
+            Log.logger.info("Scaling down image with width of " + image.getWidth(null));
+            int heightScale = 800 / image.getWidth(null);
+            int newHeight = image.getHeight(null)*heightScale;
+            if(newHeight <= 0){
+                newHeight = 800;
+            }
+            Image smallImg = image.getScaledInstance(800,
+                    newHeight,
+                    Image.SCALE_SMOOTH);
+            imageIcon = new ImageIcon(smallImg);
+        }
         JLabel img = new JLabel();
         img.setIcon(imageIcon);
         imageIcon.getImage().flush();
